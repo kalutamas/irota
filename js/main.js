d17c78f447c50af9bdb59fa6d1ff3dd99c66c01f
kalutamas <kalutamas@hotmail.com>
2016-08-28 22:04:50 +0200

    make the structure - second commmit

--- js/main.js
+++ js/main.js
@@ -1,19 +1,30 @@
-$(document).ready(function() {
-   
-
-
-
+$(document).ready(function(){
+  $('.carousel').slick({
+  	infinite: true,
+  	prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    lazyLoad: 'ondemand',
+  });
+/*Scroll to top*/
+$('.goup').click(function() {
+    $('html, body').animate({
+        scrollTop: $('body').offset().top
+    }, 1000);
+});
 });
 
+/*Toggle menu elements child visibility*/
 $( ".menu>li" ).hover(function() {
   $(this).find('ul').toggle();
 });
-
+/*
 $( "footer .up" ).click(function() {
   $('.hider').slideToggle();
 });
-
+*/
+/*Toggle visibilty of goup button*/
 $(window).scroll(function () {
 	var scrollPosition = $(this).scrollTop();
-	$('.goup').attr('class', scrollPosition < 10 ? "goup" : "goup visb");}
-);
+	$('.goup').attr('class', scrollPosition < 10 ? "goup" : "goup visb");
+});
+
